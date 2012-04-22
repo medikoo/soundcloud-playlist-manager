13b624f2e962159be27600081a6d049048de17fb
Mariusz Nowak <mariusz@medikoo.com>
2012-04-22 21:10:01 +0200

    Quotes consistency

--- test/model/text.js
+++ test/model/text.js
@@ -3,13 +3,13 @@
 module.exports = function (t, a) {
   var text = new t(' raz '), node;
 
-  a(String(text), ' raz ', 'toString');
-  a(text.trim(), 'raz', 'Inherits from String.prototype');
+  a(String(text), ' raz ', "toString");
+  a(text.trim(), 'raz', "Inherits from String.prototype");
   node = text.toDOM();
   a(node.nodeType, 3, "toDOM");
   a(node.data, ' raz ', "DOM content");
 
   text.update('dwa');
-  a(String(text), 'dwa', 'Update');
-  a(node.data, 'dwa', 'DOM update');
+  a(String(text), 'dwa', "Update");
+  a(node.data, 'dwa', "DOM update");
 };
