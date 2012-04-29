2d2f21a9c3c9371bc89a14a0a4ac8ab6705c906d
Mariusz Nowak <mariusz@medikoo.com>
2012-04-29 14:57:32 +0200

    Fixed tests for playlist so they work on real tracks

--- test/model/playlist.js
+++ test/model/playlist.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var i = require('es5-ext/lib/function/i');
+var i = require('es5-ext/lib/function/i')
+  , Track = require('../../lib/model/track');
 
 module.exports = function (t, a) {
   var playlist = new t('Title', 'Description'), node, json, invoked, history
-    , x = { _id: 'x' }, y = { _id: 'y' }, z = { _id: 'z' };
+    , x = new Track('tx', 'test', 'test', 'test')
+    , y = new Track('ty', 'test', 'test', 'test')
+    , z = new Track('tz', 'test', 'test', 'test');
 
   a(Array.isArray(playlist), true, "Is Array");
   a(String(playlist.title), 'Title', "Title");
@@ -34,20 +37,20 @@ module.exports = function (t, a) {
   a(node.data, 'New T', "DOM content");
 
   json = JSON.parse(JSON.stringify(playlist));
-  a.deep(json.data, ['x', 'z', 'y'], "toJSON: data");
+  a.deep(json.data, ['tx', 'tz', 'ty'], "toJSON: data");
   delete json.data;
   a.deep(json, { title: 'New T', description: 'New D' }, "toJSON: rest");
 
   playlist = t.fromJSON('pid', json = {
     title: 'TestTitle',
     description: 'TestDescription',
-    data: ['a', 'b', 'd']
+    data: ['tx', 'ty', 'tz']
   });
 
   a(String(playlist._id), 'pid', "fromJSON: id");
   a(String(playlist.title), 'TestTitle', "fromJSON: title");
   a(String(playlist.description), 'TestDescription', "fromJSON: description");
-  a.deep(playlist.map(Function.pluck('_id')), ['a', 'b', 'd'],
+  a.deep(playlist.map(Function.pluck('_id')), ['tx', 'ty', 'tz'],
     "fromJSON: data");
 
   a(t.get('pid'), playlist, "get");
